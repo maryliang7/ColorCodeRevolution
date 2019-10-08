b162da343fa4eec11e0d9bd94040be2f5c08bb80
Mary Liang <mary.liang96@gmail.com>
2019-10-07 20:53:31 -0700

    change arrow keys

--- src/input.js
+++ src/input.js
@@ -12,46 +12,54 @@ export default class InputHandler {
     }
     document.addEventListener("keydown", event => {
       switch (event.keyCode) {
-        case 188:
+        case 37:
+          event.preventDefault();
           staticA.pressDown("left");
           if (this.keys[67] ^ this.keys[88] ^ this.keys[90]) {
             game.checkTarget("left", this.keys);
           }
           break; 
-        case 76:
+        case 38:
+          event.preventDefault();
           staticA.pressDown("up");
           if (this.keys[67] ^ this.keys[88] ^ this.keys[90]) {
             game.checkTarget("up", this.keys);
           }
           break; 
-        case 190:
+        case 40:
+          event.preventDefault();
           staticA.pressDown("down");
           if (this.keys[67] ^ this.keys[88] ^ this.keys[90]) {
             game.checkTarget("down", this.keys);
           }
           break; 
-        case 191:
+        case 39:
+          event.preventDefault();
           staticA.pressDown("right");
           if (this.keys[67] ^ this.keys[88] ^ this.keys[90]) {
             game.checkTarget("right", this.keys);
           }
           break;
         case 67:
+          event.preventDefault();
           this.keys[67] = true;
           staticA.pressDownColor("blue");
           staticA.changeCurrentColor('blue');
           break;
         case 88:
+          event.preventDefault();
           this.keys[88] = true;
           staticA.pressDownColor("purple");
           staticA.changeCurrentColor('purple');
           break;
         case 90:
+          event.preventDefault();
           this.keys[90] = true;
           staticA.pressDownColor("navy");
           staticA.changeCurrentColor('navy');
           break;
         case 32:
+          event.preventDefault();
           game.pause();
           game.music.toggleSong()
           break;
@@ -59,16 +67,16 @@ export default class InputHandler {
     })
     document.addEventListener("keyup", event => {
       switch (event.keyCode) {
-        case 188:
+        case 37:
           staticA.pressUp("left");
           break; 
-        case 76:
+        case 38:
           staticA.pressUp("up");
           break; 
-        case 190:
+        case 40:
           staticA.pressUp("down");
           break; 
-        case 191:
+        case 39:
           staticA.pressUp("right")
           break; 
         case 67:
